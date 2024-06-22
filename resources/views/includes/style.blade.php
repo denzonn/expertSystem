14da5e400006c71dbbbc30be998588221c9af4fd
denzonn <denson753@gmail.com>
2024-06-22 10:52:08 +0800

    Build

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -3,6 +3,5 @@
 <script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
 <script src="https://cdn.ckeditor.com/ckeditor5/41.2.1/classic/ckeditor.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.11.0/dist/sweetalert2.min.css" rel="stylesheet">
-{{--  <link rel="stylesheet" href="{{ asset('build/assets/app-Dke_qurr.css') }}">  --}}
+<link rel="stylesheet" href="{{ asset('build/assets/app-2mB6GB_6.css') }}">
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-@vite('resources/css/app.css')
